9b37100494698b8711c0929ffee06ad26436bc84
Meehir Prabhakar <meehirprabhakar@gmail.com>
2026-01-27 11:19:36 +0530

    add rose pine (Soho vibes color scheme)

--- lua/plugins/colors.lua
+++ lua/plugins/colors.lua
@@ -49,4 +49,11 @@ return{
             vim.cmd.colorscheme('gruvbox-material')
         end
     },
+    {
+	"rose-pine/neovim",
+	name = "rose-pine",
+	config = function()
+		vim.cmd("colorscheme rose-pine")
+	end
+    },
 }
